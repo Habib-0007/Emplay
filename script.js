c456454767468ee432b454e27ed469277609ac1d
Habib-0007 <109099706+Habib-0007@users.noreply.github.com>
2023-03-26 23:15:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,8 +38,8 @@ loadSong(songs[songIndex]);
 
 function loadSong(song) {
     title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-    cover.src = `images/${song}.jpg`;
+    audio.src = `${song}.mp3`;
+    cover.src = `${song}.jpg`;
 }
 
 function playSong() {
